f66f86eb426592e143ec1f31c0f03268aa9ccf78
ricoisme <rico0203@msn.com>
2018-03-05 19:56:24 +0800

    fixed shell scripts

--- BuildScripts/ShellScripts/CreateService.sh
+++ BuildScripts/ShellScripts/CreateService.sh
@@ -2,8 +2,8 @@
 service=@{serviceName}@.service
 if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
 then
-systemctl stop $service
-systemctl disable $service
+sudo systemctl stop $service
+sudo systemctl disable $service
 rm -rf /usr/lib/systemd/system/$service
 else
 echo "[Unit]
@@ -24,6 +24,7 @@ WantedBy=multi-user.target" > /usr/lib/systemd/system/$service
 
 fi
 
+sudo systemctl daemon-reload
 
 
 
