19d249c2676c152c116f61ecd990394d8ea4f1bf
ricoisme <rico0203@msn.com>
2018-03-05 11:31:51 +0800

    updated description

--- BuildScripts/ShellUpdater.cs
+++ BuildScripts/ShellUpdater.cs
@@ -15,6 +15,7 @@ namespace BuildScripts
             var shellScriptsPath = $"{mainPath}/ShellScripts/CreateService.sh";
             var scriptsContent = File.ReadAllText(shellScriptsPath);
             scriptsContent = scriptsContent.Replace("${serviceName}$", $"{environment}-{serviceName}");
+            scriptsContent = scriptsContent.Replace("${Description}$", "Example ASP.NET Web API running on CentOS 7");
             scriptsContent = scriptsContent.Replace("${WorkingDirectory}$", "/home/PrimeService/AssemblyOutput");
             scriptsContent = scriptsContent.Replace("${assembly}$", "MyAPI.dll");
             scriptsContent = scriptsContent.Replace("${Environment}$", $"{environment}");
