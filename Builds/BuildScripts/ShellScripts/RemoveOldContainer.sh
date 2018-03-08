8babe7ed4e264b1edf9ebfee86c35d6cfcaf73d8
ricoisme <rico0203@msn.com>
2018-03-09 02:08:15 +0800

    added shell scripts

---
+++ Builds/BuildScripts/ShellScripts/RemoveOldContainer.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/sh
+result=$(sudo docker ps -a -q --filter ancestor=coreapp)
+
+if [[ -n "$result" ]]; then
+  docker rm $(sudo docker ps -a -q --filter ancestor=coreapp)
+  echo "removed old Containers of coreapp"
+else
+  echo "No such container"
+fi
