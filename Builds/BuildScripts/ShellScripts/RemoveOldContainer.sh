9182b250b6feb3aa0d32d5b7878f7c63e745288e
ricoisme <rico0203@msn.com>
2018-03-09 03:22:07 +0800

    modified shell scripts

--- Builds/BuildScripts/ShellScripts/RemoveOldContainer.sh
+++ Builds/BuildScripts/ShellScripts/RemoveOldContainer.sh
@@ -3,6 +3,7 @@ result=$(sudo docker ps -a -q --filter ancestor=coreapp)
 
 if [[ -n "$result" ]]; then
   sudo docker stop $(sudo docker ps -a -q --filter ancestor=coreapp)
+  echo "stopped old Containers of coreapp"
   sudo docker rm $(sudo docker ps -a -q --filter ancestor=coreapp)
   echo "removed old Containers of coreapp"
 else
