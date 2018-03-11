49ff7470b5bf0505a25990e399e64a1e3f2fdb4e
ricoisme <rico0203@msn.com>
2018-03-12 01:52:58 +0800

    fixed shell scripts

--- Builds/BuildScripts/Redis/entrypoint.sh
+++ Builds/BuildScripts/Redis/entrypoint.sh
@@ -5,4 +5,4 @@ sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /usr/local/etc/redis/sentinel.co
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /usr/local/etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /usr/local/etc/redis/sentinel.conf
 
-exec docker-entrypoint.sh redis-server /usr/local/etc/redis/sentinel.conf --sentinel
+exec /usr/local/bin/docker-entrypoint.sh redis-server /usr/local/etc/redis/sentinel.conf --sentinel
